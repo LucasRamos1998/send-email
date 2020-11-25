d26c550caa8505489f6fecf3638baaec2504f6ee
Lucas Souza <lucas.souza@redfox.tech>
2020-11-25 15:55:55 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/main/**',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
