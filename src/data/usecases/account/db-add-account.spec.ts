83ad7cae9f988f0f0f8202f563e26bd4dbc79d28
Lucas Souza <lucas.souza@redfox.tech>
2020-11-26 12:48:52 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/account/db-add-account.spec.ts
+++ src/data/usecases/account/db-add-account.spec.ts
@@ -3,21 +3,38 @@ import { AddAccountRepository } from "@/data/protocols/db/account/add-account-re
 import { addAccountParams } from "@/domain/usecases/add-account"
 import { accountModel } from "@/domain/models/account"
 
+const makeAddAccountRepository = (): AddAccountRepository => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (accountData: addAccountParams): Promise<accountModel> {
+      return Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      })
+    }
+  }
+  return new AddAccountRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbAddAccount
+  addAccountRepositoryStub: AddAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addAccountRepositoryStub = makeAddAccountRepository()
+  const sut = new DbAddAccount(addAccountRepositoryStub)
+  return {
+    sut,
+    addAccountRepositoryStub
+  }
+}
+
 describe('DbAddAccount UseCase', () => {
   test('Should call DbAddAccountRepository with correct values', async () => {
-    class DbAddAccountRepositoryStub implements AddAccountRepository {
-      async add (accountData: addAccountParams): Promise<accountModel> {
-        return Promise.resolve({
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          password: 'any_password'
-        })
-      }
-    }
-    const dbAddAccountRepositoryStub = new DbAddAccountRepositoryStub()
-    const sut = new DbAddAccount(dbAddAccountRepositoryStub)
-    const addSpy = jest.spyOn(dbAddAccountRepositoryStub, 'add')
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
     await sut.add({
       name: 'any_name',
       email: 'any_email',
