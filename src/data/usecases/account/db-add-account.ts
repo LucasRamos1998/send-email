53933a2ba11c5433136f5a15930dbf0d5a225d9d
Lucas Souza <lucas.souza@redfox.tech>
2020-11-26 13:08:36 -0300

    feat: ensure DbAddAccount calls Hasher with correct value

--- src/data/usecases/account/db-add-account.ts
+++ src/data/usecases/account/db-add-account.ts
@@ -1,12 +1,17 @@
 import { AddAccount, addAccountParams } from "@/domain/usecases/add-account";
 import { AddAccountRepository } from "@/data/protocols/db/account/add-account-repository";
 import { accountModel } from "@/domain/models/account";
+import { Hasher } from "@/data/protocols/cryptography/hasher";
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly hasher: Hasher
+  ) {}
 
-  async add (account: addAccountParams): Promise<accountModel> {
-    await this.addAccountRepository.add(account)
+  async add (accountData: addAccountParams): Promise<accountModel> {
+    await this.hasher.encrypt(accountData.password)
+    await this.addAccountRepository.add(accountData)
     return null
   }
 }
