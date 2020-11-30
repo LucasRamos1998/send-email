bbae2fca2c32899e4f58eb608f4602abdfa8debf
Lucas Souza <lucas.souza@redfox.tech>
2020-11-30 16:57:17 -0300

    feat: ensure BcryptAdapter returns a hash value on hash success

--- src/infra/db/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/db/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -3,9 +3,9 @@ import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Hasher {
   constructor (private readonly salt: number) {}
-  
+
   async hash (value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt)
-    return null
+    const hashValue = await bcrypt.hash(value, this.salt)
+    return hashValue
   }
 }
