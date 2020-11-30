0f69673b19b9a2f6d348a94fd0468aee07955365
Lucas Souza <lucas.souza@redfox.tech>
2020-11-30 16:32:05 -0300

    feat: ensure AccountSequelizeRepository return an accout on LoadByEmail success

--- src/infra/db/sequelize/account/account-sequelize-repository.ts
+++ src/infra/db/sequelize/account/account-sequelize-repository.ts
@@ -1,10 +1,11 @@
 import { Accounts } from '../models'
 import { AddAccountRepository } from "@/data/protocols/db/account/add-account-repository"
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
 import { addAccountParams } from "@/domain/usecases/add-account"
 import { accountModel } from "@/domain/models/account"
 import uuid4 from "uuid4"
 
-export class AccountSequelizeRepository implements AddAccountRepository {
+export class AccountSequelizeRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: addAccountParams): Promise<accountModel> {
     const id = uuid4()
     const newAccount = await Accounts.create({
@@ -12,5 +13,12 @@ export class AccountSequelizeRepository implements AddAccountRepository {
       id
     })
     return newAccount
+  }
+
+  async loadByEmail (email: string): Promise <accountModel> {
+    const account = await Accounts.findOne({
+      where: { email }
+    })
+    return account ? account : null
   }
 }
