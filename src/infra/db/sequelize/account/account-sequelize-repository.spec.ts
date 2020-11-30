c4b60ce8fc7197d46470436498fa26ab3301bd39
Lucas Souza <lucas.souza@redfox.tech>
2020-11-30 16:32:13 -0300

    test: ensure AccountSequelizeRepository return an accout on LoadByEmail success

--- src/infra/db/sequelize/account/account-sequelize-repository.spec.ts
+++ src/infra/db/sequelize/account/account-sequelize-repository.spec.ts
@@ -13,28 +13,47 @@ describe('Account Sequelize Repositoy', () => {
     connection.close()
   })
 
-  test('Should create a new account in database', async () => {
-    const sut = new AccountSequelizeRepository()
-    const newAccount = await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('add', () => {
+    test('Should create a new account in database', async () => {
+      const sut = new AccountSequelizeRepository()
+      const newAccount = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      expect(newAccount).toBeTruthy()
+      expect(newAccount.id).toBeTruthy()
+      expect(newAccount.name).toBe('any_name')
+      expect(newAccount.email).toBe('any_email@mail.com')
     })
-    expect(newAccount).toBeTruthy()
-    expect(newAccount.id).toBeTruthy()
-    expect(newAccount.name).toBe('any_name')
-    expect(newAccount.email).toBe('any_email@mail.com')
+  
+    test('Should throws if add throws', async () => {
+      const sut = new AccountSequelizeRepository()
+      jest.spyOn(sut, 'add').mockReturnValueOnce(Promise.reject(new Error()))
+      const promise = sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      await expect(promise).rejects.toThrow()
+    })
   })
 
-  test('Should throws if add throws', async () => {
-    const sut = new AccountSequelizeRepository()
-    jest.spyOn(sut, 'add').mockReturnValueOnce(Promise.reject(new Error()))
-    const promise = sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
+  describe('loadByEmail', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      await Accounts.create({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+
+      const sut = new AccountSequelizeRepository()
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.email).toBe('any_email@mail.com')
     })
-    await expect(promise).rejects.toThrow()
-  })
-  
+
+  })  
 })
