b8b89031e7356bac50187c754267aa64675e6b8c
Lucas Souza <lucas.souza@redfox.tech>
2020-11-30 15:26:30 -0300

    test: ensure AccountSequelizeRepository create a new account in database

---
+++ src/infra/db/sequelize/account/account-sequelize-repository.spec.ts
@@ -0,0 +1,29 @@
+import { AccountSequelizeRepository } from './account-sequelize-repository'
+import { Accounts } from '../models'
+import connection from '../database'
+
+describe('Account Sequelize Repositoy', () => {
+  beforeAll(() => {
+    Accounts.init(connection)
+  })
+  beforeEach(() => {
+    Accounts.truncate()
+  })
+  afterAll(() => {
+    connection.close()
+  })
+
+  test('Should create a new account in database', async () => {
+    const sut = new AccountSequelizeRepository()
+    const newAccount = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(newAccount).toBeTruthy()
+    expect(newAccount.id).toBeTruthy()
+    expect(newAccount.name).toBe('any_name')
+    expect(newAccount.email).toBe('any_email@mail.com')
+  })
+
+})
