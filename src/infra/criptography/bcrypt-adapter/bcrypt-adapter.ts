eecdfcdf0155c8e5cccd38d34a6ecf3e188c55d1
Lucas Souza <lucas.souza@redfox.tech>
2020-11-30 20:20:14 -0300

    refactor: ensure BcryptAdapter return a hash value

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,11 +1,12 @@
-import { Hasher } from "@/data/protocols/cryptography/hasher";
+import { Hasher } from "@/data/protocols/cryptography/hasher"
 import bcrypt from 'bcrypt'
 
 export class BcryptAdapter implements Hasher {
   constructor (private readonly salt: number) {}
 
-  async hash (value: string): Promise<string> {
+  async  hash (value: string): Promise<string> {
     const hashValue = await bcrypt.hash(value, this.salt)
     return hashValue
   }
+
 }
