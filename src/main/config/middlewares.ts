8e4c650e3b111194c2bb038214511aad35eca8c5
Lucas Souza <lucas.souza@redfox.tech>
2020-12-01 13:17:57 -0300

    feat: config cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
-import { bodyParser } from '../middlewares'
+import { bodyParser, cors } from '../middlewares'
 import { Express } from 'express'
 
 export default (app: Express) => {
   app.use(bodyParser)
+  app.use(cors)
 }
