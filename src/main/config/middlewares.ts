269a2a113fe621b30d65cd4fb500bda48dae721b
Lucas Souza <lucas.souza@redfox.tech>
2020-12-01 13:19:13 -0300

    feat: config content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
-import { bodyParser, cors } from '../middlewares'
+import { bodyParser, cors, contentType } from '../middlewares'
 import { Express } from 'express'
 
 export default (app: Express) => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
