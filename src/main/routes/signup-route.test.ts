d16c0dc468b2894bf4039b1139ad1e648983ee6d
Lucas Souza <lucas.souza@redfox.tech>
2020-12-01 17:47:38 -0300

    test: ensure route /signup returns 200

---
+++ src/main/routes/signup-route.test.ts
@@ -0,0 +1,33 @@
+import app from '../config/app'
+import { Accounts } from '@/infra/db/sequelize/models'
+import connection from '@/infra/db/sequelize/database'
+import request from 'supertest'
+
+describe('Signup Route', () => {
+  beforeAll(() => {
+    Accounts.init(connection)
+  })
+
+  beforeEach(() => {
+    Accounts.truncate()
+  })
+  
+  afterAll(() => {
+    connection.close()
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+      .post('/api/signup')
+      .send({
+        name: "any_name",
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      })
+      .expect(200)
+    })
+  })
+  
+})
