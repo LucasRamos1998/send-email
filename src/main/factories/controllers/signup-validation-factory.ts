948178383c5ac3ba86b687c3fd9d652216319555
Lucas Souza <lucas.souza@redfox.tech>
2020-12-02 14:19:47 -0300

    fix: add CompareFieldValidation

--- src/main/factories/controllers/signup-validation-factory.ts
+++ src/main/factories/controllers/signup-validation-factory.ts
@@ -2,12 +2,14 @@ import { ValidationComposite } from "@/validation/validators/validation-composit
 import { RequiredFieldValidation } from "@/validation/validators/required-field-validation";
 import { EmailValidation } from "@/validation/validators/email-validation";
 import { EmailValidatorAdapter } from "@/infra/validators/email-validator-adapter";
+import { CompareFieldsValidation } from "@/validation/validators/compare-fields-validation";
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations = []
   for (const field of ['email', 'name', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   validations.push(new EmailValidation(new EmailValidatorAdapter()))
   return new ValidationComposite(validations)
 }
