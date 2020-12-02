b6598d3de691b6c79d6655cf7babc7db15cb3bd1
Lucas Souza <lucas.souza@redfox.tech>
2020-12-02 16:40:29 -0300

    feat: ensure LoginController returns 400 if Validation return an error

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,12 +1,18 @@
-import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols";
+import { Controller, HttpRequest, HttpResponse, Validation } from "@/presentation/protocols";
 import { Authentication } from "@/domain/usecases/authentication";
-import { ok } from "@/presentation/helpers/http/http-helpers";
+import { ok, badRequest } from "@/presentation/helpers/http/http-helpers";
 
 export class LoginController implements Controller {
-  constructor (private readonly authentication: Authentication) {}
+  constructor (
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+    ) {}
 
-  async handle (request: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = request.body
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body)
+    if (error) return badRequest(error)
+    
+    const { email, password } = httpRequest.body
     const authenticationModel = await this.authentication.auth(email, password)
     return ok(authenticationModel)
   }
