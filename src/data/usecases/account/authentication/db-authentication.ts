59a7640f767cb6efd99c64cd19bb001165f9ab47
Lucas Souza <lucas.souza@redfox.tech>
2020-12-02 17:50:35 -0300

    feat: ensure DbAuthentication calls HashCompare with correct values

--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,12 +1,18 @@
 import { Authentication } from "@/domain/usecases/authentication";
 import { LoadAccountByEmailRepository } from "@/data/protocols/db/account/load-account-by-email-repository";
 import { AuthenticationModel } from "@/domain/models/authentication";
+import { HashCompare } from "@/data/protocols/cryptography/hash-compare";
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashCompare: HashCompare
+    ) {}
 
   async auth (email: string, password: string): Promise<AuthenticationModel> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (!account) return null
+
+    await this.hashCompare.compare(password, account.password)
   }
 }
