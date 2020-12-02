f56a1e268bf32dabb3b2104d0d34c563c265b0da
Lucas Souza <lucas.souza@redfox.tech>
2020-12-02 16:19:55 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -2,26 +2,45 @@ import { LoginController } from './login-controller'
 import { Authentication } from "@/domain/usecases/authentication"
 import { AuthenticationModel } from "@/domain/models/authentication"
 
+const mockRequest = {
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+}
+
+const makeAuthentication = (): Authentication => {
+  class AuthenticationStub implements Authentication {
+    async auth (email: string, password: string): Promise<AuthenticationModel> {
+      return Promise.resolve({
+        id: 'any_id',
+        name: 'any_name',
+        token: 'any_token'
+      })
+    }
+  }
+  return new AuthenticationStub()
+}
+
+type SutTypes = {
+  sut: LoginController
+  authenticationStub: Authentication
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationStub = makeAuthentication()
+  const sut = new LoginController(authenticationStub)
+  return {
+    sut,
+    authenticationStub
+  }
+}
+
 describe('Login Controller', () => {
   test('Should calls Authentication with correct values', () => {
-    class AuthenticationStub implements Authentication {
-      async auth (email: string, password: string): Promise<AuthenticationModel> {
-        return Promise.resolve({
-          id: 'any_id',
-          name: 'any_name',
-          token: 'any_token'
-        })
-      }
-    }
-    const authenticationStub = new AuthenticationStub()
-    const sut = new LoginController(authenticationStub)
+    const { sut, authenticationStub } = makeSut()
     const authSpy = jest.spyOn(authenticationStub, 'auth')
-    sut.handle({
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    })
+    sut.handle(mockRequest)
     expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
   })
 })
