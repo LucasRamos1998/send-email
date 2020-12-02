709b4d25d74f7c05190d573ce6fb4dd4fd3328a3
Lucas Souza <lucas.souza@redfox.tech>
2020-12-02 16:29:20 -0300

    feat: ensure LoginController return an AccountModel if Authentication succeeds

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,12 +1,13 @@
 import { Controller, HttpRequest, HttpResponse } from "@/presentation/protocols";
 import { Authentication } from "@/domain/usecases/authentication";
+import { ok } from "@/presentation/helpers/http/http-helpers";
 
 export class LoginController implements Controller {
   constructor (private readonly authentication: Authentication) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     const { email, password } = request.body
-    this.authentication.auth(email, password)
-    return null
+    const authenticationModel = await this.authentication.auth(email, password)
+    return ok(authenticationModel)
   }
 }
