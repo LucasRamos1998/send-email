b0c4e605e45e76886422873909cbb93c3dff6db1
Lucas Souza <lucas.souza@redfox.tech>
2020-12-03 16:55:13 -0300

    refactor: move files to a folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,5 +1,5 @@
 import { makeSignupValidation } from './signup-validation-factory'
-import { makeDbAddAccount } from '../usecases/db-add-account-factory'
+import { makeDbAddAccount } from '../../usecases/db-add-account-factory'
 import { Controller } from '@/presentation/protocols/controller'
 import { SignupController } from '@/presentation/controllers/signup/signup-controller'
 
--- src/main/routes/signup-route.ts
+++ src/main/routes/signup-route.ts
@@ -1,6 +1,6 @@
 import { Router} from "express"
 import { adaptRoute } from '../adapters/express-router-adapter'
-import { makeSignupController } from '../factories/controllers/signup-controller-factory'
+import { makeSignupController } from '../factories/controllers/signup/signup-controller-factory'
 
 export default (router: Router) => {
   router.post('/signup', adaptRoute(makeSignupController()))
