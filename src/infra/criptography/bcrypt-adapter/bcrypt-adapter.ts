c5e4b4d41e51f3b9f83457878ae151d33d7c0d75
Lucas Souza <lucas.souza@redfox.tech>
2020-12-03 13:33:05 -0300

    feat: ensure BcryptAdapter returns true if compare returns true

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -11,8 +11,8 @@ export class BcryptAdapter implements Hasher, HashCompare {
   }
 
   async compare (value: string, hash: string ): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return null
+    const isValid =  await bcrypt.compare(value, hash)
+    return isValid
   }
 
 }
