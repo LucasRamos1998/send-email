e79981748b5424a2ea6575f381f9d03d640b6a44
Lucas Souza <lucas.souza@redfox.tech>
2020-12-03 17:33:44 -0300

    test: ensure LoginRoute returns 401 if email or password is invalid

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -19,7 +19,7 @@ describe('Signup Route', () => {
   })
 
   describe('POST /login', () => {
-    test('Should return 200 on signup', async () => {
+    test('Should return 200 on login success', async () => {
       const password = await hash('any_password', 12)
       await Accounts.create({
         id: 'any_id',
@@ -34,6 +34,23 @@ describe('Signup Route', () => {
         password: 'any_password'
       })
       .expect(200)
+    })
+
+    test('Should return 401 on email or password is invalid', async () => {
+      const password = await hash('any_password', 12)
+      await Accounts.create({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password
+      })
+      await request(app)
+      .post('/api/login')
+      .send({
+        email: 'any_email@mail.com',
+        password: 'other_password'
+      })
+      .expect(401)
     })
   })
   
